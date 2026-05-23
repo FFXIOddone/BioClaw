f36b640c2aee2e535e129b94492440aad0defae9
FFXIOddone <225563018+FFXIOddone@users.noreply.github.com>
2026-05-22 21:51:46 -0400

    chore: scaffold bioscaffold package

---
+++ bioscaffold/__init__.py
@@ -0,0 +1,6 @@
+"""BioScaffold OS foundation package."""
+
+from bioscaffold.cell import BioCell
+from bioscaffold.types import CellRole, LifecyclePhase
+
+__all__ = ["BioCell", "CellRole", "LifecyclePhase"]
